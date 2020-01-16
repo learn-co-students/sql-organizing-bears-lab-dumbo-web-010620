9a52187a3664602dca9ffd8dc3434007e6d5aa5f
github username <github email address>
2020-01-16 14:44:36 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+-- (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender CHAR(1), color TEXT, temperament TEXT, alive BOOLEAN)
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 10, "F", "peachy", "vivacious", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 15, "F", "black", "nervous", 1);
+INSERT INTO bears VALUES (4, "Seargent Brown", 25, "M", "brown", "accomodating", 0); -- aka One Leggy
+INSERT INTO bears VALUES (5, "Melissa", 25, "M", "brown", "accomodating", 0);
+INSERT INTO bears VALUES (6, "Grinch", 25, "M", "brown", "accomodating", 0);
+INSERT INTO bears VALUES (7, "Wendy", 25, "F", "brown", "accomodating", 0);
+INSERT INTO bears VALUES (8, null, 5, "M", "brown", "clinically depressed", 1);
+
+
+-- don't forget semicolons!!!
+-- null => empty value!
+-- it's not copying my values to seed.sql
